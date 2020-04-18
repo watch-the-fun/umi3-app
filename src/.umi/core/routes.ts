3e85f38694a2a756b99ab74f6a44e7c62660aaf6
geyx <bruce.g@163.com>
2020-04-18 22:51:28 +0800

    init commit

---
+++ src/.umi/core/routes.ts
@@ -0,0 +1,19 @@
+import { ApplyPluginsType } from '/Users/geyx/Documents/github_repo/umi3-app/node_modules/@umijs/runtime';
+import { plugin } from './plugin';
+
+const routes = [
+  {
+    "path": "/",
+    "component": require('@/pages/index').default,
+    "exact": true
+  }
+];
+
+// allow user to extend routes
+plugin.applyPlugins({
+  key: 'patchRoutes',
+  type: ApplyPluginsType.event,
+  args: { routes },
+});
+
+export { routes };
